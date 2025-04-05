92dc33d4ee2f6731c8c6b7ffc17e07c8dc79d7e9
mamad <you@example.com>
2025-04-05 11:22:02 +0330

    create home page with its components

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+
+    return (
+        <>
+            <div className="grid grid-cols-3 gap-4 max-w-4xl mx-auto mt-8 ">
+                <a href='/my-links' className="bg-blue-500 p-6 text-white rounded-lg shadow-md" >My Links</a>
+                <a href='/create-new-url' className="bg-green-500 p-6 text-white  rounded-lg shadow-md">Create New URL</a>
+                <a href='/my-profile' className="bg-green-500 p-6 text-white  rounded-lg shadow-md">My Profile</a>
+            </div>
+        </>
+    );
+}
