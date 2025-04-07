c0d19d133db256a0afb7e1638dc1c6f66a401765
mamad <you@example.com>
2025-04-07 12:05:53 +0330

    add logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,29 @@
-export default function NavBar(){
-    return(
+import api from '../services/domain.ts'
+
+export default function NavBar() {
+
+    const logout = async () => {
+        await api.post("/user/logout");
+        alert("Logout successful");
+    };
+
+    return (
         <header className="w-full p-4 bg-white shadow-md">
             <nav className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold text-gray-800">Shortener Link</h1>
                 <ul className="flex space-x-4">
                     <li><a href='/home' className="text-gray-600 hover:text-gray-800">Home</a></li>
+                    <li>
+                        <a
+                            href='/'
+                            className="text-gray-600 hover:text-gray-800"
+                            onClick={logout}
+                        >
+                            Logout
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
